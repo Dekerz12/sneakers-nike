3a7cb09f049480834664fb73bd561437568ab0a5
Sedrick Reyes <sedrickreyes.dev@gmail.com>
2023-07-19 11:18:43 +0800

    update animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
-import SneakersProvider from './SneakerContext';
-import SidebarButton from './SidebarButton';
-import Dropdown from './Dropdown';
-import ThemePicker from './ThemePicker';
-import Sidebar from './SideBar';
-import SneakerList from './SneakerList';
-import Total from './Total';
-import './App.css';
+import SneakersProvider from "./SneakerContext";
+import SidebarButton from "./SidebarButton";
+import Dropdown from "./Dropdown";
+import ThemePicker from "./ThemePicker";
+import Sidebar from "./Sidebar";
+import SneakerList from "./SneakerList";
+import Total from "./Total";
+import "./App.css";
 
 function App() {
   return (
-    <div className='container-fluid'>
-      <div className='flex headings title'>
+    <div className="container-fluid">
+      <div className="flex headings title">
         <h1>Nike</h1>
         <ThemePicker />
       </div>
 
       <SneakersProvider>
-        <section style={{ marginTop: '5em' }}>
-          <hgroup className='flex space-between'>
+        <section style={{ marginTop: "5em" }}>
+          <hgroup className="flex space-between">
             <Total />
-            <div className='flex buttons'>
+            <div className="flex buttons">
               <SidebarButton />
               <Dropdown />
             </div>
           </hgroup>
         </section>
 
-        <section className='flex content'>
+        <section className="flex content">
           <Sidebar />
           <SneakerList />
         </section>
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,23 +1,21 @@
-import { useSneakers } from './SneakerContext';
-import ColorFilter from './categories/ColorFilter';
-import BrandFilter from './categories/BrandFilter';
-import ShoeConditionFilter from './categories/ShoeConditionFilter';
-import SizeFilter from './categories/SizeFilter';
+import { useSneakers } from "./SneakerContext";
+import ColorFilter from "./categories/ColorFilter";
+import BrandFilter from "./categories/BrandFilter";
+import ShoeConditionFilter from "./categories/ShoeConditionFilter";
+import SizeFilter from "./categories/SizeFilter";
 export default function Sidebar() {
   const { isSidebarOpen, query, setQuery } = useSneakers();
   const handleSearch = (event) => {
     setQuery(event.target.value);
   };
 
-  if (isSidebarOpen) {
-    return (
-      <div className='sidebar'>
-        <input type='text' value={query} onChange={handleSearch} autoFocus />
-        <ColorFilter />
-        <BrandFilter />
-        <ShoeConditionFilter />
-        <SizeFilter />
-      </div>
-    );
-  }
+  return (
+    <div className="sidebar" style={{ width: isSidebarOpen ? "15em" : "0" }}>
+      <input type="text" value={query} onChange={handleSearch} autoFocus />
+      <ColorFilter />
+      <BrandFilter />
+      <ShoeConditionFilter />
+      <SizeFilter />
+    </div>
+  );
 }
